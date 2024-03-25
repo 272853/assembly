003c69e69da3b3d7d7a5baa2fb9be0811dee6843
272853 <272853@student.pwr.edu.pl>
2024-03-25 20:14:06 +0100

    wybieranie operacji

--- kalkulator_duzych_liczb.asm
+++ kalkulator_duzych_liczb.asm
@@ -1,16 +1,52 @@
 [bits 32]
 
-push 'p'
-call [ebx+4]
-add esp, 4
-push 'o'
-call [ebx+4]
-push 'd'
-call [ebx+4]
-add esp, 4
-push 'a'
-call [ebx+4]
-push 'j'
-call [ebx+4]
+
+
+call wyswietl
+db "Podaj operacje:",0 
+wyswietl:
+call [ebx+3*4]
+add esp, 8
+
+call [ebx+2*4]
+push eax
+cmp esp, '+'
+jne nie_dodawanie
+;dodawanie-->
+call wypisz_dodaje
+db 'dodaje', 0xa, 0 ;db wzuca miezdzy kod te dane
+wypisz_dodaje:
+call [ebx+3*4] 
 add esp, 4
 
+
+nie_dodawanie:
+cmp esp, '-'
+jne mnozenie
+;odejmowanie-->
+call wypisz_odejmuje
+db 'odejmuje', 0xa, 0 ;db wzuca miezdzy kod te dane
+wypisz_odejmuje:
+call [ebx+3*4] 
+add esp, 4
+
+
+mnozenie:
+cmp esp, '*'
+jne inne
+;mnoznenie-->
+call wypisz_mnoze
+db 'mnoze', 0xa, 0 ;db wzuca miezdzy kod te dane
+wypisz_mnoze:
+call [ebx+3*4] 
+add esp, 4
+
+
+inne:
+call wypisz_inne
+db 'inne', 0xa, 0 ;db wzuca miezdzy kod te dane
+wypisz_inne:
+call [ebx+3*4] 
+add esp, 4
+
+call [ebx]
