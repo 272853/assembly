ec2abc93c612358ab4a3911c0810225ed44870a5
272853 <272853@student.pwr.edu.pl>
2024-03-25 20:44:45 +0100

    lekko poprawione wybieranie instrukcji

--- kalkulator_duzych_liczb.asm
+++ kalkulator_duzych_liczb.asm
@@ -6,47 +6,50 @@ call wyswietl
 db "Podaj operacje:",0 
 wyswietl:
 call [ebx+3*4]
-add esp, 8
+add esp, 12
 
 call [ebx+2*4]
 push eax
-cmp esp, '+'
+cmp esp, 'd'
 jne nie_dodawanie
 ;dodawanie-->
 call wypisz_dodaje
-db 'dodaje', 0xa, 0 ;db wzuca miezdzy kod te dane
+db 'dodaje', 0xa, 0 
 wypisz_dodaje:
 call [ebx+3*4] 
 add esp, 4
+jmp koniec
 
 
 nie_dodawanie:
-cmp esp, '-'
+cmp esp, 'o'
 jne mnozenie
 ;odejmowanie-->
 call wypisz_odejmuje
-db 'odejmuje', 0xa, 0 ;db wzuca miezdzy kod te dane
+db 'odejmuje', 0xa, 0 
 wypisz_odejmuje:
 call [ebx+3*4] 
 add esp, 4
-
+jmp koniec
 
 mnozenie:
-cmp esp, '*'
+cmp esp, 'm'
 jne inne
 ;mnoznenie-->
 call wypisz_mnoze
-db 'mnoze', 0xa, 0 ;db wzuca miezdzy kod te dane
+db 'mnoze', 0xa, 0 
 wypisz_mnoze:
 call [ebx+3*4] 
 add esp, 4
-
+jmp koniec
 
 inne:
 call wypisz_inne
-db 'inne', 0xa, 0 ;db wzuca miezdzy kod te dane
+db 'inne', 0xa, 0 
 wypisz_inne:
 call [ebx+3*4] 
 add esp, 4
+jmp koniec
 
+koniec:
 call [ebx]
